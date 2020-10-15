cbe7e69a16b0c57a0d36577761ff67beaccbb20a
KozaX <55134615+KozaX@users.noreply.github.com>
2020-10-15 15:37:04 +0200

    add accessor decorator

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,20 @@ const Logger = (logString: string) => {
   }
 }
 
+function WithTemplate(template: string, hookdId: string) {
+  return function(constructor: any) {
+    const hookEl = document.getElementById(hookdId);
+    const person = new constructor();
+    if (hookEl) {
+      console.log('Rendering template')
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent = person.name;
+    }
+  }
+}
 
-
-@Logger('Logging person')
+@Logger('Logging')
+@WithTemplate('<h1>This was set by a decorator factory</h1>', 'app')
 class Person {
   name = 'Pedro';
 
@@ -21,3 +32,41 @@ class Person {
 
 const person = new Person();
 console.log(person)
+
+// ---
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log('Property decorator');
+  console.log(target, propertyName);
+}
+
+function secondLog(target: any, name: string, descriptor: PropertyDescriptor ) {
+  console.log('Accessor decorator');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @secondLog
+  set price(val: number){
+    if(val > 0){
+      this._price = val;
+    } else {
+      throw new Error('Invalid price - must be >= 0')
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1+tax);
+  }
+}
